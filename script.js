8456ad60dd00bdfd146bd96fb9e96118479abb18
Alys <alice.g.cooper@gmail.com>
2022-04-22 17:29:10 -0400

    Set up individ dice buttons

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ let diceNumber = document.querySelector("#diceNumber");
 let diceSide = document.querySelector("#diceSide");
 
 
-function rollDice (diceNumber,diceSide) {
-    // diceNumber is how many times the operation needs to loop
-    for (let i = 0; i < diceNumber; i++) {
-    //diceSide is the type of Die it is - example, a d4 is 4, etc., needs
-    //to choose a random number between 1 and diceSide
-    console.log(Math.floor(Math.random() * diceSide));
-    }
+function rollDice () {
+  return(Math.floor(Math.random() * diceSide));
 }
 
-let btn = document.querySelector('#rollDice');
-let para = document.createElement('div');
-btn.appendChild(para);
-para.textContent = "Here is a test.";
+for (let i = 0; i < diceNumber; i++) (console.log(rollDice()));    
+
+    //diceSide is the type of Die it is - example, a d4 is 4, etc., needs
+    //to choose a random number between 1 and diceSide
+
+let btn = document.querySelectorAll(".rollDice");
+
+btn.forEach
+
+btn.addEventListener('click', function (e) {
+    console.log(rollDice());
+    let para = document.createElement('div');
+    btnDiv.appendChild(para);
+    para.textContent = `Here is a test.`;
+  }
+);
+
+let btnDiv = document.querySelector('#rollDice');
 
 //append result of each roll to results div
